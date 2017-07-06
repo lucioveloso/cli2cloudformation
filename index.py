b3d5678de6149ce88154d8f02980167282d53b53
glucio <glucio@amazon.com>
2017-07-06 21:39:44 +0200

    Updating function

--- index.py
+++ index.py
@@ -3,33 +3,38 @@ import shlex
 import subprocess
 import requests
 
+
 def lambda_handler(event, context):
-    nullReturn = json.loads("{}")
+    null_return = json.loads("{}")
     if ('RequestType' not in event or
         'ResourceProperties' not in event):
-        sendResponseCfn(event, context, "FAILED", nullReturn, "Cannot identify RequestType or ResourceProperties." )
+        sendResponseCfn(event, context, "FAILED", null_return, "Cannot identify RequestType or ResourceProperties." )
         return
 
-    properties = event['ResourceProperties'];
+    properties = event['ResourceProperties']
+    command_request_type = "CliCommand" + event['RequestType']
 
-    if ("CreateAsDefault" in properties and
-        properties['CreateAsDefault'] == "False"):
-        commandRequestType = "CliCommand" + event['RequestType'];
-    else:
-        commandRequestType = "CliCommandCreate"
+    # Use action create as default if not action update or delete is specified.
+    if("CreateAsDefault" in properties and
+        properties["CreateAsDefault"] == "True" and
+            (command_request_type not in properties or
+             properties[command_request_type] == "")):
+        command_request_type = "CliCommandCreate"
 
-    if (commandRequestType in properties and
-        properties[commandRequestType] != ""):
+    if (command_request_type in properties and
+        properties[command_request_type] != ""):
         try:
-            output = run_cmd("wrapper " + properties[commandRequestType])
-            r = nullReturn if output is None else json.loads(output)
+            output = run_cmd("aws " + properties[command_request_type])
+            r = null_return if output is None else json.loads(output)
             sendResponseCfn(event, context, "SUCCESS", r, "Command Executed.")
         except Exception as e:
-            sendResponseCfn(event, context, "FAILED", nullReturn, "Failed to execute the command.")
+            print(e)
+            sendResponseCfn(event, context, "FAILED", null_return, "Failed to execute the command.")
     else:
-        sendResponseCfn(event, context, "SUCCESS", nullReturn, commandRequestType + " has no action.")
+        sendResponseCfn(event, context, "SUCCESS", null_return, command_request_type + " has no action.")
 
     return True
+
 
 def run_cmd(command):
     command = shlex.split("/bin/bash -c '" + command + "'")
@@ -37,8 +42,9 @@ def run_cmd(command):
     output, stderr = process.communicate()
     return output
 
+
 def sendResponseCfn(event, context, responseStatus, responseData, reason):
-    responseBody = {'Status': responseStatus,
+    response_body = {'Status': responseStatus,
                     'Reason': 'Log stream name: ' + context.log_stream_name,
                     'PhysicalResourceId': context.log_stream_name,
                     'StackId': event['StackId'],
@@ -47,7 +53,7 @@ def sendResponseCfn(event, context, responseStatus, responseData, reason):
                     'Data': responseData}
     print("Log Reason: " + reason)
     try:
-        req = requests.put(event['ResponseURL'], data=json.dumps(responseBody))
+        requests.put(event['ResponseURL'], data=json.dumps(response_body))
     except Exception as e:
         print(e)
         raise
